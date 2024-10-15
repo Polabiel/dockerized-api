b032cfbc7503d075517b41e1dd44c657d192c61b
Gabriel <bielgabrieloliveira77@gmail.com>
2024-10-15 20:19:26 -0300

    Feat: create postMapping account

--- src/main/java/com/cotuca/account/controllers/AccountController.java
+++ src/main/java/com/cotuca/account/controllers/AccountController.java
@@ -1,15 +1,17 @@
 package com.cotuca.account.controllers;
 
+import com.cotuca.account.AccountApplication;
 import com.cotuca.account.models.Account;
 import com.cotuca.account.repositories.AccountRepository;
 import com.cotuca.account.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController @RequestMapping("/accounts")
 public class AccountController {
@@ -23,5 +25,18 @@ public class AccountController {
     public ResponseEntity<List<Account>> getAccounts() {
         List<Account> accounts = accountRepository.findAll();
         return accounts.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(accounts);
+    }
+
+    @GetMapping("/{number}")
+    public ResponseEntity<Account> getAccountByNumber(@PathVariable Integer number) {
+        return accountRepository.findById(number)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/create/account")
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+        Account savedAccount = accountRepository.save(account);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedAccount);
     }
 }
