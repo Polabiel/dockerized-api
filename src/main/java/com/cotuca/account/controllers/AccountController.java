e0af204df811308b162560c42c4475a54c1a5fa0
Gabriel Oliveira <bielgabrieloliveira77@gmail.com>
2024-10-22 22:59:04 +0000

    continue feat in classroom

--- src/main/java/com/cotuca/account/controllers/AccountController.java
+++ src/main/java/com/cotuca/account/controllers/AccountController.java
@@ -2,10 +2,12 @@ package com.cotuca.account.controllers;
 
 import com.cotuca.account.AccountApplication;
 import com.cotuca.account.models.Account;
+import com.cotuca.account.models.Transaction;
 import com.cotuca.account.repositories.AccountRepository;
 import com.cotuca.account.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +15,8 @@ import javax.swing.*;
 import java.util.List;
 import java.util.Optional;
 
-@RestController @RequestMapping("/accounts")
+@RestController
+@RequestMapping("/accounts")
 public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
@@ -38,5 +41,38 @@ public class AccountController {
     public ResponseEntity<Account> createAccount(@RequestBody Account account) {
         Account savedAccount = accountRepository.save(account);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedAccount);
+    }
+
+    @PutMapping("/number")
+    public ResponseEntity<Account> updateAccount(@PathVariable Integer number, @RequestBody Account account) {
+        return accountRepository.findById(number)
+                .map(__account -> {
+                    __account.setNumber(account.getNumber());
+                    __account.setOwner(account.getOwner());
+                    __account.setBalance(account.getBalance());
+                    return ResponseEntity.ok(accountRepository.save(__account));
+                })
+                .orElseGet(() -> {
+                    return ResponseEntity.status(HttpStatus.CREATED).body(accountRepository.save(account));
+                });
+    }
+
+    @GetMapping("/{number}")
+    public ResponseEntity<List<Transaction>> getListBalanceById(@PathVariable Integer number) {
+        List<Transaction> transactions = transactionRepository.findByAccountNumber(number);
+
+        return transactions.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(transactions);
+    };
+
+    @PostMapping("/{number}/deposit")
+    public ResponseEntity<Transaction> makeDeposit(@PathVariable Integer number, @RequestBody Transaction transaction) {
+        return accountRepository.findById(number)
+                .map(account -> {
+                    account.setBalance(account.getBalance() + transaction.getAmount());
+                    accountRepository.save(account);
+                    Transaction savedTransaction = transactionRepository.save(transaction);
+
+                    return ResponseEntity.status(HttpStatus.CREATED).body(savedTransaction);
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/cotuca/account/repositories/TransactionRepository.java
+++ src/main/java/com/cotuca/account/repositories/TransactionRepository.java
@@ -1,7 +1,11 @@
 package com.cotuca.account.repositories;
 
 import com.cotuca.account.models.Transaction;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+    List<Transaction> findByAccountNumber(Integer number);
 }
